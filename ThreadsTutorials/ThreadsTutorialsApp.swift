2e6da3efda2e09568dd9918805ed8b044a37301d
Enes Ceylan <enesceylan_6699@hotmail.com>
2023-12-13 12:43:47 +0300

    Initial Commit

---
+++ ThreadsTutorials/ThreadsTutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsTutorialsApp.swift
+//  ThreadsTutorials
+//
+//  Created by Enes Ceylan on 13.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsTutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
